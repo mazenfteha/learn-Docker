7a5fd7864ee4b105cc8b5c4215c3cfff56b2d5d5
Mazen Mohamed <mazn34903@gmail.com>
2023-08-16 18:36:37 +0300

    Add files via upload
    website Visits

---
+++ Read.md
@@ -0,0 +1,6 @@
+## to run docker compose :  
+>docker compose up -d   
+
+## to stop docker compose  :  
+>docker compose down  
+
---
+++ index.js
@@ -0,0 +1,20 @@
+const express =  require('express');
+const redis =  require('redis');
+
+const app = express();
+
+const client = redis.createClient({
+    host: 'redis',
+    port: 6379
+});
+client.set('visitsCounter',0);
+app.get('/', (req, res)=>{
+    client.get('visitsCounter', (err,visitsCounter)=>{
+        res.send('Visits Counter :' + visitsCounter);
+        client.set('visitsCounter', parseInt(visitsCounter) +1);
+    })
+})
+
+app.listen(8080, ()=>{
+    console.log('Listening on port 8080')
+})
